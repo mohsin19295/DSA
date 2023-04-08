5f07a08867a6049c46428ff4629873f822e9dd18
mohsin19295 <mohsin19295@gmail.com>
2023-04-09 02:13:30 +0530

    contest 3 added

--- Unit_1/contest_2.js
+++ Unit_1/contest_2.js
@@ -1,69 +1,32 @@
 /*
-? Sum Related Problem
+? Breaking a Record
 
-You are given a number N, you need to find sum of all the even numbers which are less than or equal to N.
+You are provided the highest runs N, ever scored in a cricket match till now, which is current highest record.
 
-Input: 4
-Output: 6
+You are also given current runs, M scored by Sachin Tendulkar in a today's match.
 
-Input: 3
-Output: 2
+If Sachin breaks current record in that match, print 'Broken'.
+If Sachin could not break that record, print 'Not Yet'.
+If Sachin score exactly as current highest record, print 'Wao'.
+
+Input: 240 200
+Output: Not Yet
 */
 
-const n = 10;
+const [n, m] = [240, 200]
 
-console.log(
-  new Array(n)
-    .fill()
-    .map((_, i) => i + 1)
-    .filter((e) => e % 2 == 0)
-    .reduce((a, c) => a + c, 0)
-);
-
-/*
-? Division Task
-
-Let’s say you derive a variable X which is equal to the floor of a number you get when 32 is divided by N. 
-
-In case X is 0, print 'Too Low'
-In case it is not possible to generate a valid number, print '-1'
-In all other cases, print X
-
-Input: 4
-Output: 8
-
-Input: 3
-Output: 10
-*/
-
-const N = 3;
-
-console.log(N > 32 ? "Too Low" : N <= 0 ? -1 : Math.floor(32 / N));
+console.log(m > n ? 'Broken' : m < n ? 'Not Yet' : 'Wao');
 
 
 /*
-? Masai School! Hurray!
+? Equal to 42 or not
 
-You are provided an integer N. For all numbers in the range [1, N], including N you have to print a string as per the following rules:
+You are given an array of N integers. You need to print 'Yes' if 42 is present in that array, else print 'No'.
 
-Print 'Masai School' if the number is a multiple of bith 5 and 7.
-Print 'Masai', if the number is a multiple of 7 only.
-Print 'School' if the number is a multiple of 5 only.
-Else in all other cases print 'Hurray!'
-
-Input: 7
-Output: 
-Hurray!
-Hurray!
-Hurray!
-Hurray!
-School
-Hurray!
-Masai
+Input: 5, [3, 7, 0, 9, 8]
+Output: No
 */
 
-const m = 7;
+const [size, arr] = [5, [3, 7, 0, 9, 8]]
 
-new Array(m).fill().forEach((_, e) => {
-  e = e + 1;  console.log(e % 7 === 0 && e % 5 === 0 ? 'Masai School' : e % 7 === 0 ? "Masai" : e % 5 === 0 ? 'School' : 'Hurray!');
-})
+console.log(arr.includes(42) ? 'Yes' : 'No');
