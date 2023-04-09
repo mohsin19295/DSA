85261b7eec6f1ac6715ab24fafbe868ef77a43df
mohsin19295 <mohsin19295@gmail.com>
2023-04-09 12:17:13 +0530

    more evaluation ques added

--- Unit_1/Evaluation/evaluation_1.js
+++ Unit_1/Evaluation/evaluation_1.js
@@ -13,6 +13,87 @@ const name = 'Mohsin'
 
 console.log(`Hello ${name}`)
 
+
+/*
+? Power of 4
+
+You are given a number stored in a variable with the name 'nums'. You have to find the power of 4, equals 3 * 3 * 3 * 3 = 81, which is the required answer. Therefore, the output is 81.
+
+Input: 3
+Outpit: 81
+*/
+
+const num = 3;
+
+console.log(num ** 4)
+
+/*
+? Sum Two Equations
+
+You are given two numbers, stored in a variable with the names, x and y. You have to find the sum of x and y, such that 
+x = (x * 5) + (y * 3)
+y = (x * 7) + (y * 4)
+
+Input: 2 3
+Outpit: 45
+*/
+
+const [x, y] = [2, 3]
+
+console.log(((x * 5) + (y * 3)) + ((x * 7) + (y * 4)))
+
+
+/*
+? Logical And
+
+You are given two numbers stored in variables with the name X and Y. Print the result of the following operation: X > 15 && Y > 20
+
+Input: 16 21
+Ouput: true
+*/
+
+const [X, Y] = [16, 21]
+
+console.log(X > 15 && Y > 20)
+
+
+/*
+? Divisible by 5
+
+You are given a number stored in the variable with the name Num. If the value is divisible by 5, print 'Yes', else print 'No'.
+
+Input: 45
+Output: Yes
+*/
+
+
+const Num = 45;
+
+console.log(Num % 5 === 0 ? 'Yes' : 'No');
+
+
+/*
+? Range of 2
+
+You are given two numbers stored in the variable with the names 'left' and 'right' respectively. You have to print all the numbers in the range of [left, right] including the value stored in left and right such that the current number is double of the previous number.
+
+For example, consider the value stored in left = 2, and the calue stored in right = 10, then the required output will be '2 4 8'.
+
+Note: Print the output on every new line.
+
+Input: 4 10
+Output: 4
+8
+*/
+
+
+const [left, right] = [8, 32]
+
+for (let i = left; i <= right; i *= 2){
+    console.log(i)
+}
+
+
 /*
 ? Second Maximum
 
