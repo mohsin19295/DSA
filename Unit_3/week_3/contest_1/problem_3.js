d54c5ea68c6aba953eccaa2585890a4c95c15d3d
mohsin19295 <mohammad19295@gmail.com>
2023-12-01 13:10:35 +0530

    week_3 contest_1 problem_3 added

--- Unit_3/week_3/contest_1/problem_3.js
+++ Unit_3/week_3/contest_1/problem_3.js
@@ -21,9 +21,21 @@ Output: 0 3 5 8 9
 */
 
 function runProgram(input) {
-    input = input.split('\n');
-    
+  input = input.split('\n');
+  const n = +input[0];
+  const arr = input[1].split(' ').map(Number);
+  console.log(bubbleSort(arr, n).join(' '));
 }
+
+const bubbleSort = (arr, n) => {
+  if (n === 1) return arr;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  return bubbleSort(arr, n - 1);
+};
 if (process.env.USERNAME === 'ubuntu') {
   runProgram(`5
 3 5 0 9 8`);
